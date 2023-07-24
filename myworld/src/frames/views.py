3384202916bd70cdb68b35aee7debb1dd41e4c4e
Rohith <nagarjuna.rohith3@gmail.com>
2023-07-24 22:31:54 +0530

    rendering context in a template

--- myworld/src/frames/views.py
+++ myworld/src/frames/views.py
@@ -12,7 +12,13 @@ def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html", {})
 	# print(args, kwargs)
 	# print(request.user)
- # 	return render(request, "contact.html", {})
+ 	# return render(request, "contact.html", {})
 
 def detail_view(request, *args, **kwargs):
-	return render(request, "detail.html", {})
+    my_context = {
+        "my_description": "abc this is about us",
+        "my_num": 123,
+        "my_list": [1313, 4231, 312, "Abc"]
+    }
+    return render(request, "detail.html", my_context)
+
