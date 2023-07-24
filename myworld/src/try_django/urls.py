4ed76a216d64a84bfcaf4fd80e0bfc7d22a777c4
Rohith <nagarjuna.rohith3@gmail.com>
2023-07-25 02:27:18 +0530

    in app urls and name spacing

--- myworld/src/try_django/urls.py
+++ myworld/src/try_django/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for try_django project.
+"""trydjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,21 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from frames.views import mainScreen_view, contact_view, detail_view
-from products.views import (product_details_view, product_create_view, product_delete_view )
+from frames.views import main_view, contact_view, detail_view
 
-# urlpatterns = [
-# 	path('contact/', contact_view),	
-# 	path('detail/', detail_view),	
-# 	path('product/', product_details_view),	
-# 	path('create/', product_create_view),	
-# 	path('', mainScreen_view, name='main screen'),	
-#     path('admin/', admin.site.urls),
-# ]
 
 urlpatterns = [
-     path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
-     path('products/', product_list_view, name='product-list'),
+    path('products/', include('products.urls')),
+    path('', main_view, name='home'),
+    path('about/<int:id>/', about_view, name='product-detail'),
+    path('contact/', contact_view),
+    path('admin/', admin.site.urls),
 ]
