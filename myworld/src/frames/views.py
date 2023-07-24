c466e4434aa948cd4109b4d0a09167a84b2303c6
Rohith <nagarjuna.rohith3@gmail.com>
2023-07-24 20:51:49 +0530

    url routing and requests

--- myworld/src/frames/views.py
+++ myworld/src/frames/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def mainScreen_view(*args, **kwargs):
+def mainScreen_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
 	return HttpResponse("<h1>This is main screen/ home screen</h1>")
+
+
+def contact_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	return HttpResponse("<h1>This is contact screen</h1>")
