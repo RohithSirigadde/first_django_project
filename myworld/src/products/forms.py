cf574817e1ec8efa3d180eaa8e4ea01753dead75
Rohith <nagarjuna.rohith3@gmail.com>
2023-07-25 01:29:53 +0530

    form validation methods

--- myworld/src/products/forms.py
+++ myworld/src/products/forms.py
@@ -1,20 +1,54 @@
 from django import forms
 
+
 from .models import Product
 
+
 class ProductForm(forms.ModelForm):
-	class Meta:
-		model = Product
-		fields= [
-			'tag',
-			'report',
-			'fare'
-		]
+    tag       = forms.CharField(label='', 
+                    widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    email	  = forms.EmailField()
+    report = forms.CharField(
+                        required=False, 
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class-name two",
+                                    "id": "my-id-for-textarea",
+                                    "rows": 20,
+                                    'cols': 120
+                                }
+                            )
+                        )
+    fare       = forms.DecimalField(initial=199.99)
+    
+    class Meta:
+        model = Product
+        fields = [
+            'tag',
+            'report',
+            'fare'
+        ]
+
+
+    def clean_tag(self, *args, **kwargs):
+    	tag = self.cleaned_data.get('tag')
+    	if not "ABCD" in tag:
+    		raise forms.ValidationError("This is not a valid title")
+    	if not "ae" in tag:
+    		raise forms.ValidationError("This is not a valid title")
+    	return tag
+
+    def clean_email(self, *args, **kwargs):
+    	tag = self.cleaned_data.get("email")
+    	if not email.endswith("com"):
+    		raise forms.ValidationError("This is not a valid email")
+    	return tag
 
 
 class RawProductForm(forms.Form):
     tag       = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
-    report    = forms.CharField(
+    report = forms.CharField(
                         required=False, 
                         widget=forms.Textarea(
                                 attrs={
--- myworld/src/products/views.py
+++ myworld/src/products/views.py
@@ -4,19 +4,19 @@ from .forms import ProductForm, RawProductForm
 # Create your views here.
 from .models import Product
 
-def product_create_view(request):
-	my_form = RawProductForm()
-	if request.method == "POST":
-		my_form = RawProductForm(request.POST)
-		if my_form.is_valid():
-			print(my_form.cleaned_data)
-			Product.objects.create(**my_form.cleaned_data)
-		else:
-			print(my_form.errors)
-	context = {
-		"form" : my_form
-	}
-	return render(request, "products/product_create.html", context)
+# def product_create_view(request):
+# 	my_form = RawProductForm()
+# 	if request.method == "POST":
+# 		my_form = RawProductForm(request.POST)
+# 		if my_form.is_valid():
+# 			print(my_form.cleaned_data)
+# 			Product.objects.create(**my_form.cleaned_data)
+# 		else:
+# 			print(my_form.errors)
+# 	context = {
+# 		"form" : my_form
+# 	}
+# 	return render(request, "products/product_create.html", context)
 
 
 # def product_create_view(request):
@@ -28,21 +28,21 @@ def product_create_view(request):
 # 	context = {}
 # 	return render(request, "products/product_create.html", context)
 
-# def product_create_view(request):
-# 	form = ProductForm(request.POST or None)
-# 	if form.is_valid():
-# 		form.save()
-# 		form = ProductForm
+def product_create_view(request):
+	form = ProductForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		form = ProductForm
 
-# 	obj = Product.objects.get(id=1)
-# 	# context = {
-# 	# 'tag':obj.tag,
-# 	# 'report':obj.report
-# 	# }
-# 	context = {
-# 		'form': form
-# 	}
-# 	return render(request, "products/product_create.html", context)
+	obj = Product.objects.get(id=1)
+	# context = {
+	# 'tag':obj.tag,
+	# 'report':obj.report
+	# }
+	context = {
+		'form': form
+	}
+	return render(request, "products/product_create.html", context)
 
 def product_details_view(request):
 	obj = Product.objects.get(id=1)
